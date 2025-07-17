94c23ad7021427c1fd2d8953f4e059552a747626
Als1925 <jules.best.als@gmail.com>
2025-07-18 01:34:36 +0200

    crédit

--- scripts/levels/credits.gd
+++ scripts/levels/credits.gd
@@ -1,10 +1,13 @@
 extends Control
 
 func _ready() -> void:
-	$switch.start()
-	
+	await get_tree().create_timer(2).timeout
+	$AnimationPlayer.play("crédits")
 	
 
-func _on_switch_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/start_menu.tscn")
 	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://scenes/levels/start_menu.tscn")

72dfa72e52e0edf8b179adf636994f8a210a9b7a
Als1925 <jules.best.als@gmail.com>
2025-07-17 22:23:07 +0200

    tel

--- scripts/dead_screen.gd
+++ scripts/dead_screen.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 func _ready() -> void:
+	global.is_dax_speek = true
 	$VrmNulDax3.play()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+
+func _on_vrm_nul_dax_3_finished() -> void:
+	global.is_dax_speek = false

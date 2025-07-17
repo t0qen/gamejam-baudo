88d8c51de0fdb66a5fc28ac285cb5b0b48179c11
Als1925 <jules.best.als@gmail.com>
2025-07-17 19:36:33 +0200

    deadscreen and usb key

--- scripts/dead_screen.gd
+++ scripts/dead_screen.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	$VrmNulDax3.play()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
--- scripts/levels/level_2_ascenceur.gd
+++ scripts/levels/level_2_ascenceur.gd
@@ -3,6 +3,8 @@ extends Node2D
 var is_step_1_complete : bool = false
 var tremble : bool = true
 var can_step_2 : bool = true
+var body_in_step1 : bool = false
+var body_in_vent : bool = false
 
 func _ready() -> void:
 	await get_tree().create_timer(1).timeout
@@ -19,20 +21,16 @@ func _ready() -> void:
 	$hide_etage.hide()
 	$Arrow2.show()
 	
-
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("action") and global.player_press_e:
+	if Input.is_action_just_pressed("action") and global.player_press_e and body_in_step1:
+		print("step1")
+		$Arrow1.show()
+		$Arrow2.hide()
+		is_step_1_complete = true
+		$AsceuseurKey.hide()
+		tremble = true
+	if Input.is_action_just_pressed("action") and global.player_press_e and body_in_vent:
 		get_tree().change_scene_to_file("res://scenes/levels/level_3_ventilations.tscn")
-
-func _on_step_1_area_entered(area: Area2D) -> void:
-	print("step1")
-	print(area)
-	$Arrow1.show()
-	$Arrow2.hide()
-	is_step_1_complete = true
-	$AsceuseurKey.hide()
-	tremble = true
-
 
 func _on_step_2_body_entered(body: Node2D) -> void:
 	print("step2")
@@ -41,6 +39,7 @@ func _on_step_2_body_entered(body: Node2D) -> void:
 		$block_gate/CollisionShape2D.call_deferred("set", "disabled", false)
 		$hide_etage.show()
 		$Timer.start()
+		$enemies.hide()
 		while tremble == true:
 			CameraManager.shake(0.5,  1.5, Vector2(50, 100), 0.1)
 			await get_tree().create_timer(0.5).timeout
@@ -54,9 +53,25 @@ func _on_timer_timeout() -> void:
 
 func _on_vent_3_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
+		body_in_vent = true
 		global.player_press_e = true
 
 
 func _on_vent_3_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
+		body_in_vent = false
+		global.player_press_e = false
+	
+
+
+func _on_step_1_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		body_in_step1 = true
+		global.player_press_e = true
+	
+
+
+func _on_step_1_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		body_in_step1 = false
 		global.player_press_e = false

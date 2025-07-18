6acb1249d00f9f689f3326bb3b4ca6b18100d40a
Als1925 <jules.best.als@gmail.com>
2025-07-18 19:11:36 +0200

    last changes

--- scripts/levels/level_4_salle_info.gd
+++ scripts/levels/level_4_salle_info.gd
@@ -2,13 +2,25 @@ extends Node2D
 
 func _ready() -> void:
 	global.is_dax_speek = true
-	$BlagueDax3.play()
+	$BlagueDax7.play()
 
-func _on_timer_timeout() -> void:
-	Transition.transition()
-	await Transition.on_transition_finished
-	get_tree().change_scene_to_file("res://scenes/pcmain.tscn")
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("action") and global.player_press_e:
+		await get_tree().create_timer(1.5).timeout
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_file("res://scenes/pcmain.tscn")
 
 
-func _on_blague_dax_3_finished() -> void:
+func _on_blague_dax_7_finished() -> void:
 	global.is_dax_speek = false
+
+
+func _on_vent_3_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		global.player_press_e = true
+
+
+func _on_vent_3_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		global.player_press_e = false

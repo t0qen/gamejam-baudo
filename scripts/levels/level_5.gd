0e0f8788ed6d4750210e3825b7421ab7b55322e4
Als1925 <jules.best.als@gmail.com>
2025-07-18 16:46:40 +0200

    level5 UPGRADE

--- scripts/levels/level_5.gd
+++ scripts/levels/level_5.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 func _ready() -> void:
-	$Timer.start()
+	pass
 
-func _on_timer_timeout() -> void:
-	Transition.transition()
-	await Transition.on_transition_finished
-	get_tree().change_scene_to_file("res://scenes/levels/boss_fight.tscn")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_file("res://scenes/levels/boss_fight.tscn")

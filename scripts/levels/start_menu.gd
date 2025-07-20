dd28413013c80a76c7ffc326236853eef41d752e
Aymeric-devv🧨 <mybeautifuladress@gmail.com>
2025-07-20 11:30:02 +0200

    tqt

--- scripts/levels/start_menu.gd
+++ scripts/levels/start_menu.gd
@@ -1,10 +1,11 @@
-extends Control
+extends Node2D
 
 func  _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	$Menu.play()
 	
 func _on_start_pressed() -> void:
+	print("PRESSED")
 	Transition.transition()
 	await Transition.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/levels/cinematic_1.tscn")

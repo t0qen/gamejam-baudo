ffeed45c68f8e71be31896385fcd443aa5fba25e
Aymeric-devv🧨 <mybeautifuladress@gmail.com>
2025-07-20 11:20:45 +0200

    Added start menu and improved credits

--- scripts/levels/start_menu.gd
+++ scripts/levels/start_menu.gd
@@ -2,7 +2,15 @@ extends Control
 
 func  _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Menu.play()
+	
 func _on_start_pressed() -> void:
 	Transition.transition()
 	await Transition.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/levels/cinematic_1.tscn")
+
+
+func _on_credits_pressed() -> void:
+	Transition.transition()
+	await Transition.on_transition_finished
+	get_tree().change_scene_to_file("res://scenes/levels/credits.tscn")
